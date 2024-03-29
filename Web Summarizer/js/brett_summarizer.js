df599fd7adb75f2020983555a433bffab9186013
Tri11Paragon <brettmaster1@gmail.com>
2024-03-29 15:26:58 -0400

    SYNC ajax requests from main thread have been depreciated

--- Web Summarizer/js/brett_summarizer.js
+++ Web Summarizer/js/brett_summarizer.js
@@ -4,12 +4,12 @@ var requested_url = "";
 // handler for fetch request
 // updates innerhtml of the results tag to whatever the summary is. Does nothing if summary is not complete.
 // will reset requested to false.
-function handle_fetch_results()
+function handle_fetch_results(request_obj)
 {
-    if (this.status != 200)
+    if (request_obj.status != 200)
         return;
 
-    const json = JSON.parse(this.responseText);
+    const json = JSON.parse(request_obj.responseText);
 
     // bad response, somehow we got here?
     if (!json.has_summary){
@@ -39,12 +39,18 @@ function handle_update_estimate()
     element.innerHTML = "Estimated Time Remaining: " + json.days + " Days " + json.hours + " Hours " + json.minutes + " Minutes " + json.seconds + " Seconds";
 }
 
+function noop(){}
+
 // helper function for sending a fetch request (checks if the summary is complete)
-function send_fetch_request(url)
+// will run the on_complete function after the request has returned (SYNC ajax requests from main thread have been depreciated)
+function send_fetch_request(url, on_complete = noop)
 {
     const fetch_request = new XMLHttpRequest();
-    fetch_request.onload = handle_fetch_results;
-    fetch_request.open("POST", "https://cosc4p02.tpgc.me/s/fetch", false);
+    fetch_request.onload = function(){
+        handle_fetch_results(this);
+        on_complete();
+    };
+    fetch_request.open("POST", "https://cosc4p02.tpgc.me/s/fetch", true);
     fetch_request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     fetch_request.send(url);
 }
@@ -75,24 +81,24 @@ function summarize_url()
     requested_url = url;
 
     // do a sync request to check if the url already exists, if it does we really don't need to wait the 5 seconds for the update, we can just do it now.
-    send_fetch_request(url);
-
-    if (!requested)
+    send_fetch_request(url, function() {
+        if (!requested)
         return;
 
-    // send the actual summary request
-    const summarizer_request = new XMLHttpRequest();
-    summarizer_request.open("POST", "https://cosc4p02.tpgc.me/s/request", true);
-    summarizer_request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    summarizer_request.send(url);
+        // send the actual summary request
+        const summarizer_request = new XMLHttpRequest();
+        summarizer_request.open("POST", "https://cosc4p02.tpgc.me/s/request", true);
+        summarizer_request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        summarizer_request.send(url);
 
-    // send a request for a time estimate. server side MIGHT use the url to determine a more accurate estimate
-    // for now it currently use returns the average time of the last 10, no accounting for character count.
-    const estimate_request = new XMLHttpRequest();
-    estimate_request.onload = handle_update_estimate;
-    estimate_request.open("POST", "https://cosc4p02.tpgc.me/s/estimate", true);
-    estimate_request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    estimate_request.send(url);
+        // send a request for a time estimate. server side MIGHT use the url to determine a more accurate estimate
+        // for now it currently use returns the average time of the last 10, no accounting for character count.
+        const estimate_request = new XMLHttpRequest();
+        estimate_request.onload = handle_update_estimate;
+        estimate_request.open("POST", "https://cosc4p02.tpgc.me/s/estimate", true);
+        estimate_request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        estimate_request.send(url);
+    });
 }
 
 setInterval(check_for_completion, 5000);
