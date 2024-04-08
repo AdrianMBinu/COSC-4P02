ce83e10348ce0c2374ffc018991e6519b339f34b
Maulik Mann <maulikmann@gmail.com>
2024-04-08 18:18:55 -0400

    cleanup

--- Web Summarizer/js/firebase-init.js
+++ Web Summarizer/js/firebase-init.js
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
 import { getAuth, GoogleAuthProvider, TwitterAuthProvider, GithubAuthProvider, OAuthProvider } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
 
